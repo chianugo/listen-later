1818fecb5e38aa91fe7749c0a48ff2669a018b6e
cko003 <cko003@bucknell.edu>
2023-07-12 21:03:04 -0400

    Initial Commit

---
+++ listen-later/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  listen-later
+//
+//  Created by Kizito Ononuju on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Test")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ listen-later/listen_laterApp.swift
@@ -0,0 +1,17 @@
+//
+//  listen_laterApp.swift
+//  listen-later
+//
+//  Created by Kizito Ononuju on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct listen_laterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
